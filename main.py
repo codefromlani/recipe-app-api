1ca7ddeac028f03a28285b44964fcff8c46ec85d
codefromlani <rodiathammed48@gmail.com>
2025-06-22 16:32:34 +0100

    feat(cron): add 14-minute keep-alive ping using schedule and requests

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from datetime import datetime
+import schedule
+import time
+import requests
+import os
 
 from db.database import engine, Base
 from api.favorite import router
@@ -25,3 +29,24 @@ async def health_check():
         "message": "Application is healthy.",
         "timestamp": datetime.now().isoformat()
     }
+
+
+API_URL = os.getenv("API_URL") 
+
+def ping_api():
+    try:
+        response = requests.get(API_URL, timeout=5)
+        if response.status_code == 200:
+            print(f"[{datetime.now().isoformat()}] GET request sent successfully")
+        else:
+            print(f"[{datetime.now().isoformat()}] GET request failed with status code", response.status_code)
+    except Exception as e:
+        print(f"[{datetime.now().isoformat()}] Error while sending request:", e)
+
+schedule.every(14).minutes.do(ping_api)
+
+print("Cron job started: Pinging every 14 minutes...")
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
